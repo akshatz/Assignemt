fbaeb96ea912d5bc5cc7833139d90504c6382e35
Akshat Zala <akshatzala@gmail.com>
2020-01-11 12:26:21 +0530

    Added Cart module

--- cart_app/models.py
+++ cart_app/models.py
@@ -11,28 +11,27 @@ class Cart(models.Model):
 payment_id = models.CharField(max_length=100, null=True)
 
 
-def add_to_cart(self, book_id)
+def add_to_cart(self, book_id):
     book = Book.objects.get(pk=book_id)
     try:
         preexisting_order = BookOrder.objects.get(book=book, cart=self)
         preexisting_order.quantity += 1
         preexisting_order.save()
 
-    except:
-        BookOrder.DoesNotExist
-    new_order = BookOrder.objects.create(
+    except BookOrder.DoesNotExist:
+        new_order = BookOrder.objects.create(
         book=book,
         cart=self,
         quantity=1
-    )
-    new_order.save()
-    except: BookOrder.DoesNotExist:
-    new_order = BookOrder.objects.create(
-        book=book,
-        cart=self,
-        quantity=1
-    )
-    new_order.save()
+        )
+        new_order.save()
+    except BookOrder.DoesNotExist:
+        new_order = BookOrder.objects.create(
+            book=book,
+            cart=self,
+            quantity=1
+        )
+        new_order.save()
 
 
 def remove_from_cart(self, book_id):
@@ -41,9 +40,9 @@ def remove_from_cart(self, book_id):
         preexisting_order = BookOrder.objects.get(book=book, cart=self)
         if preexisting_order.quantity > 1:
             preexisting_order.quantity -= 1
-        preexisting_order.save()
+            preexisting_order.save()
         else:
-        preexisting_order.delete()
+            preexisting_order.delete()
     except BookOrder.DoesNotExist:
         pass
 
@@ -51,5 +50,5 @@ def remove_from_cart(self, book_id):
 class BookOrder(models.Model):
     book = models.ForeignKey(Book)
     cart = models.ForeignKey(Cart)
-    quantity = mdoels.IntegerField(null=True)
+    quantity = models.IntegerField(null=True)
     payment_type = models.CharField(max_length=100, null=True)
